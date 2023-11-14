baec0193119245832d70fe4b8bc0ff5177b827e9
Aishwarya Kawale <136805991+aishwarya0714@users.noreply.github.com>
2023-11-14 14:33:44 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,11 +9,19 @@ function App() {
   function decreaseCount() {
     setState(state - 1);
   }
+  let time = new Date().toLocaleTimeString();
+  const [getTime, setTime] = useState();
+  function timeFun() {
+    setTime(time);
+  }
   return (
     <div className="container">
       <h1>{state}</h1>
       <button onClick={increaseCount}>+</button>
       <button onClick={decreaseCount}>-</button>
+
+      <h1>{getTime}</h1>
+      <button onClick={timeFun}>get time</button>
     </div>
   );
 }
